44b747f2935e2dba048ed86d6a1464a0211c0360
Saphal Dhakal <86939471+Saphal56@users.noreply.github.com>
2022-11-18 06:20:31 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Spotify ");
+let songs =[
+    {
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+        {songName: "Hello", filePath:"song/1.mp3", coverpath:"covers/1.jpg"},
+]
+let audioElement = new Audio('1.mp3');
+audioElement.play();
